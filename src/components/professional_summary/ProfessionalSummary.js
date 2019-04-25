4066580a1c45381843dea70fb905680953ac5d84
Saravanaselvan SP <Saravanaselvan.SP@iff.com>
2019-04-25 12:36:26 +0530

    updated professional summary

--- src/components/professional_summary/ProfessionalSummary.js
+++ src/components/professional_summary/ProfessionalSummary.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ProfessionalSummary = (props) => {
   const summary = [
     "Having 10 years of experience in Web Development",
-    "Exposure to Web App development with Jquery, Angular JS, Angular 2+, Ruby on Rails & Express JS",
-    "Experience in working with Agile delivery model with focus on testing within iterations",
-    "Experienced in working with Continuous performance delivery model using Jenkins",
-    "Have set up AWS server and deployed sample application with Websockets",
-    "Have the ability to learn new technologies quickly and solve problems",
-    "Looking for an opportunity to work with Javascript Stack. Though I have good experience with Angular 2+, I am interested to explore React JS ecosystem",
+    "Exposure to Web App development with Jquery, Angular JS, Angular 2+, Ruby on Rails & Express JS.",
+    "Experience in working with Agile delivery model with focus on testing within iterations.",
+    "Experienced in working with Continuous performance delivery model using Jenkins.",
+    "Hands on knowledge of setting up server with AWS.",
+    "Have the ability to learn new technologies quickly and solve problems.",
+    "Looking for an opportunity to work with Javascript Stack.",
   ];
   return (
     <section id="professional_summary">
